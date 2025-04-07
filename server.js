f8b04aa58d2b54eeffb4418d222aee130f543a67
Ombre Negro <sandi.popovic@adriaticvalley.com>
2025-04-07 16:39:45 +0200

    Mongoose test schema

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app.js';
+dotenv.config();
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB);
 
-dotenv.config({ path: './config.env' });
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
 
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
